c9992f94f8008adaded0b18939084ea5a79ab2f7
JuvitaSaini09 <juvitasaini@gmail.com>
2021-08-26 22:56:30 +0530

    bug:solved error

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const billAmount = document.querySelector("#billAmount");
-
 const cashGiven = document.querySelector("#cashGiven");
 const errorMessage = document.querySelector("#errorMessage");
-const countOfNotess=document.querySelector(".countOfNotes");
+const countOfNotess = document.querySelectorAll(".countOfNotes");
 const checkButton = document.querySelector("#checkButton");
 
 const notes = [2000, 500, 100, 20, 10, 5, 1];
@@ -12,25 +11,25 @@ const notes = [2000, 500, 100, 20, 10, 5, 1];
 function OnClickHandler() {
     messageHide();
 
-    if (billAmount.value > 0) {
-        
-        if (cashGiven.value>=billAmount.value) {
-    
-            const cashbackAmount=cashGiven.value-billAmount.value;
+    if (Number(billAmount.value) > 0) {
+
+        if (Number(cashGiven.value) >= Number(billAmount.value))
+         {
+            const cashbackAmount = cashGiven.value - billAmount.value;
             calculateNotes(cashbackAmount);
 
         }
-         else {
-             console.log("bro")
+       else
+        {
             showError("The given cash is less !!");
-              }
+        }
 
-    }
- 
- else {
+    } 
+    else
+     {
         showError("Enter a valid number !!");
-       }  
-    
+    }
+
 
 }
 
@@ -45,16 +44,13 @@ function messageHide() {
     errorMessage.style.display = "none";
 }
 
-function calculateNotes(cashbackAmount)
-{
-     
-for(let i=0;i<notes.length;i++)
-{
-    const totalNotes=Math.trunc(cashbackAmount/notes[i]);
-   cashbackAmount =Math.trunc(cashbackAmount%notes[i]);
+function calculateNotes(cashbackAmount) {
 
-   countOfNotess.innerText=totalNotes;
+    for (let i = 0; i < notes.length; i++) {
+        const totalNotes = Math.trunc(cashbackAmount / notes[i]);
+        cashbackAmount = Math.trunc(cashbackAmount % notes[i]);
+        countOfNotess[i].innerText = totalNotes;
 
-}
+    }
 }
 checkButton.addEventListener("click", OnClickHandler);
